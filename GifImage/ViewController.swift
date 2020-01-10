8f8303584fad066af6172e126aa5ad0607bb88ce
Mauricio Cousillas <cousillas806@gmail.com>
2020-01-10 11:26:53 -0300

    initial commit

--- GifImage/ViewController.swift
+++ GifImage/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import SwiftGifOrigin
+
+extension UIImageView {
+  func loadGif(from url: URL) {
+    guard let data = try? Data(contentsOf: url) else {
+      print("Failed to load url")
+      return
+    }
+
+    let gifImage = UIImage.gif(data: data)
+    image = gifImage
+  }
+}
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
+    // Do any additional setup after loading the view       view.backgroundColor = .white
 
+    let gifURL = URL(string: "https://media.giphy.com/media/12NUbkX6p4xOO4/giphy.gif")!
+
+    let imageView = UIImageView()
+    imageView.loadGif(from: gifURL)
+
+    imageView.contentMode = .scaleAspectFit
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(imageView)
+
+    NSLayoutConstraint.activate([
+      imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      imageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+      imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+      imageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+    ])
+  }
 
 }
 
---
+++ README.md
@@ -0,0 +1,6 @@
+GifPlayer
+===============================
+
+Small proof of concept of an gif player using image views.
+
+<img src="./2020-01-10&#32;11.18.26.gif" />
