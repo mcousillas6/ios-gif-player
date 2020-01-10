aae596d5aa26c2e5456ca8edd968468b5dead9c5
Mauricio Cousillas <cousillas806@gmail.com>
2020-01-10 10:49:16 -0300

    Initial Commit

---
+++ GifImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GifImage
+//
+//  Created by Mauricio Cousillas on 1/10/20.
+//  Copyright © 2020 Mauricio Cousillas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
